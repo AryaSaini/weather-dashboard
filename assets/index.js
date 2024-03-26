5e03e70ae724f6356d7518e0336e8bd95142563b
Arya Saini <arya30012@gmail.com>
2024-03-26 01:07:29 -0400

    made index.js have more than 1 function to run everything to improve code structure

--- assets/index.js
+++ assets/index.js
@@ -1,68 +1,93 @@
 const forecast = document.getElementById('forecast');
 const searchButton = document.getElementById("searchButton");
 const searchBar = document.getElementById('searchBar');
-const futureForecast = document.getElementById('futureForecast')
+const searchHistory = document.getElementById('searchHistory')
 
-// document.getElementById('searchButton').addEventListener('click', function () {
-//     var query = document.getElementById('searchBar').value;
-//     getWeather(query)
-// })
+searchButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const query = searchBar.value.trim();
+    if (query !== "") {
+        getWeather(query)
+    } else {
+        alert("Please enter a city")
+    }
+})
 
 function getWeather(query) {
-    
-    query = query || document.getElementById("searchBar").value;
-    localStorage.setItem("lastSearch", query)
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=b6549afb1f5e06e23ab640064bd6dd7e&units=imperial&cnt=5`)
-        .then(function (response) {
+    const apiKey = 'b6549afb1f5e06e23ab640064bd6dd7e';
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${apiKey}&units=imperial&cnt=6`;
+
+    fetch(url)
+        .then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
             return response.json();
         })
-        .then(function (data) {
-            var cityName = data.city.name;
-            var date = data.list[0].dt_txt;
-            var icon = data.list[0].weather[0].icon;
-            var temperature = data.list[0].main.temp + 'F';
-            var humidity = data.list[0].main.humidity + '%';
-            var windSpeed = data.list[0].wind.speed + 'mph';
-            console.log(`http://openweathermap.org/img/w/${icon}.png`)
 
-            var currentWeather = document.createElement('div');
-            currentWeather.innerHTML = `
-                <h2> ${cityName}<h2>
-                <h3>${date}<h3>
-                <img src="http://openweathermap.org/img/w/${icon}.png"/>
-                <p>Temperature:${temperature}<p>
-                <p>Humidity:${humidity}<p>
-                <p>Wind Speed:${windSpeed}<p>`;
-            forecast.appendChild(currentWeather);
-
-            var futureForecast = document.getElementById('futureForecast')
-
-            console.log(data)
-            for (var i = 1; i < data.list.length; i++) {
-                var icon = data.list[i].weather[0].icon;
-                var temperature = data.list[i].main.temp + 'F';
-                var humidity = data.list[i].main.humidity + '%';
-                var windSpeed = data.list[i].wind.speed + 'mph';
-
-                var forecastContainer = document.createElement('div')
-                forecastContainer.innerHTML = `<img src="http://openweathermap.org/img/w/${icon}.png"/>
-                <p>Temperature:${temperature}<p>
-                <p>Humidity:${humidity}<p>
-                <p>Wind Speed:${windSpeed}<p>`;
-
-                console.log(forecastContainer);
-
-
-                futureForecast.appendChild(forecastContainer);
+        .then(data => {
+            if (data.cod === '404') {
+                displayError('Enter a valid City');
+                return;
             }
-        }
-        )
+            displayWeather(data);
+            addToSearchHistory(query);
+        })
 }
 
-searchButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    const query = searchBar.value
-})
+function displayWeather(data) {
+    clearContent(forecast)
+
+    var cityName = data.city.name;
+    var date = data.list[0].dt_txt;
+    var icon = data.list[0].weather[0].icon;
+    var temperature = data.list[0].main.temp + 'F';
+    var humidity = data.list[0].main.humidity + '%';
+    var windSpeed = data.list[0].wind.speed + 'mph';
+
+    const currentWeatherContainer = document.createElement('div');
+    currentWeatherContainer.innerHTML = `
+        <h2>${cityName}</h2>
+        <h3>${date}</h3>
+        <img src="http://openweathermap.org/img/w/${icon}.png"/>
+        <p>Temperature: ${temperature}</p>
+        <p>Humidity: ${humidity}</p>
+        <p>Wind Speed: ${windSpeed}</p>`;
+    forecast.appendChild(currentWeatherContainer);
+
+    displayFutureForecast(data);
+}
+
+function displayFutureForecast(data) {
+    clearContent(document.getElementById('futureForecast'));
+    const futureForecastContainer = document.getElementById('futureForecast');
+
+
+    for (let i = 1; i < data.list.length; i++) {
+        const forecast = data.list[i];
+        const icon = forecast.weather[0].icon;
+        const temperature = forecast.main.temp + 'F';
+        const humidity = forecast.main.humidity + '%';
+        const windSpeed = forecast.wind.speed + 'mph';
+
+        const forecastContainer = document.createElement('div');
+        forecastContainer.innerHTML = `
+            <img src="http://openweathermap.org/img/w/${icon}.png"/>
+            <p>Temperature: ${temperature}</p>
+            <p>Humidity: ${humidity}</p>
+            <p>Wind Speed: ${windSpeed}</p>`;
+        futureForecastContainer.appendChild(forecastContainer);
+    }
+}
+
+function clearContent(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function addToSearchHistory(query) {
+    const searchItem = document.createElement('div');
+    searchItem.textContent = query;
+    searchHistory.appendChild(searchItem);
+}
